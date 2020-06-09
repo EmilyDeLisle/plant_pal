33a300c481b5af99140609efd87bf168ec13e465
EmilyDeLisle <emily.delisle@gmail.com>
2020-06-09 15:33:11 -0700

    Creating reusable event section for plant dialog

--- src/plants/plant-list/components/ListRow.tsx
+++ src/plants/plant-list/components/ListRow.tsx
@@ -1,7 +1,6 @@
 import React, { ReactElement } from 'react'
 import Card from '@material-ui/core/Card'
 import Typography from '@material-ui/core/Typography'
-import moment from 'moment'
 import IconButton from '@material-ui/core/IconButton'
 import Tooltip from '@material-ui/core/Tooltip'
 import DoneIcon from '@material-ui/icons/Done'
--- src/plants/plant-list/components/PlantDialog.tsx
+++ src/plants/plant-list/components/PlantDialog.tsx
@@ -1,15 +1,30 @@
 import React, { useState } from 'react'
 import moment from 'moment'
 import Button from '@material-ui/core/Button'
+import Card from '@material-ui/core/Card'
+import CloseIcon from '@material-ui/icons/Close'
 import Dialog from '@material-ui/core/Dialog'
 import DialogActions from '@material-ui/core/DialogActions'
 import DialogContent from '@material-ui/core/DialogContent'
 import DialogTitle from '@material-ui/core/DialogTitle'
-import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
 import MenuItem from '@material-ui/core/MenuItem'
 import Select from '@material-ui/core/Select'
-import { Plant } from '../../../models'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
+import { Plant, PlantEventType } from '../../../models'
 import { formatDate } from './plantHelpers'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    titleCard: {
+      color: theme.palette.primary.contrastText,
+    },
+    titleText: {
+      textShadow: '2px 2px 6px rgba(0, 0, 0, 0.5)',
+    },
+  })
+)
 
 export interface PlantDialogProps {
   open: boolean
@@ -35,17 +50,70 @@ export const PlantDialog = ({ open, handleClose, plant }: PlantDialogProps) => {
     wateringDates,
     fertilizingDates,
   } = plant
+  const classes = useStyles()
   const [period, setPeriod] = useState(3)
   const avgWateringInterval = getAvgWateringInterval(period)
 
+  const renderEventsSection = (eventType: PlantEventType, lastEventDate: string | undefined) => {
+    const title = eventType === PlantEventType.FERTILIZE ? 'Fertilizer' : 'Water'
+    const action = eventType === PlantEventType.FERTILIZE ? 'fertilize' : 'water'
+
+    return (
+      <Card>
+        <div className="plant-dialog-event-section__card">
+          <div className="plant-dialog-event-section__row">
+            <Typography variant="h5">{title}</Typography>
+            <Button color='primary' variant="contained">{`${action} plant today`}</Button>
+          </div>
+          {eventType === PlantEventType.WATER && avgWateringInterval && (
+            <div className="plant-dialog-event-section__row plant-dialog-event-section__body">
+              <Typography display="inline">
+                {`Watered (on average) every ${avgWateringInterval} day${
+                  avgWateringInterval !== 1 && 's'
+                } `}
+              </Typography>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={period}
+                onChange={({ target: { value } }) => setPeriod(value as number)}
+              >
+                {periodOptions.map((opt) => (
+                  <MenuItem value={opt.value}>{opt.label}</MenuItem>
+                ))}
+              </Select>
+            </div>
+          )}
+          <Typography>{`Last ${action}ed: ${formatDate(lastEventDate)}`}</Typography>
+        </div>
+      </Card>
+    )
+  }
+
   return (
-    <Dialog open={open} onClose={handleClose}>
-      <DialogTitle>{name}</DialogTitle>
+    <Dialog open={open} onClose={handleClose} maxWidth="sm" fullWidth>
+      <div className={`${classes.titleCard} plant-dialog__title-card`}>
+        <div className="plant-dialog__title-card-text">
+          <Typography className={classes.titleText} variant="h4">
+            {name}
+          </Typography>
+          <Typography className={classes.titleText}>Plant alternate name</Typography>
+        </div>
+        <div className="plant-dialog__title-card-close-button">
+          <IconButton color="inherit" edge="end" onClick={handleClose}>
+            <CloseIcon />
+          </IconButton>
+        </div>
+      </div>
       <DialogContent>
+        {renderEventsSection(PlantEventType.WATER, lastWateredDate)}
+        {/* <Typography variant="h5">Water</Typography>
         {avgWateringInterval && (
           <>
             <Typography display="inline">
-              {`Watered every ${avgWateringInterval} day${avgWateringInterval !== 1 && 's'} `}
+              {`Watered (on average) every ${avgWateringInterval} day${
+                avgWateringInterval !== 1 && 's'
+              } `}
             </Typography>
             <Select
               labelId="demo-simple-select-label"
@@ -58,7 +126,7 @@ export const PlantDialog = ({ open, handleClose, plant }: PlantDialogProps) => {
               ))}
             </Select>
           </>
-        )}
+        )} */}
         <Typography>{`Last watered: ${formatDate(lastWateredDate)}`}</Typography>
         {!!wateringDates.length && (
           <Typography variant="body2" color="textSecondary">
@@ -70,6 +138,7 @@ export const PlantDialog = ({ open, handleClose, plant }: PlantDialogProps) => {
             {moment(date).format('MMM D, YYYY')}
           </Typography>
         ))}
+        <Typography variant="h5">Fertilizer</Typography>
         <Typography>{`Last fertilized: ${formatDate(lastFertilizedDate)}`}</Typography>
         {!!fertilizingDates.length && (
           <Typography variant="body2" color="textSecondary">
@@ -83,9 +152,7 @@ export const PlantDialog = ({ open, handleClose, plant }: PlantDialogProps) => {
         ))}
       </DialogContent>
       <DialogActions>
-        <Button onClick={handleClose} color="primary">
-          Close
-        </Button>
+        <Button color="primary">Action</Button>
       </DialogActions>
     </Dialog>
   )
