62fcd9800cb0c7bf860c4816713d0188a41da818
EmilyDeLisle <emily.delisle@gmail.com>
2020-06-16 18:27:29 -0700

    Updated gitignore

--- src/firebase/init.ts
+++ src/firebase/init.ts
@@ -1,16 +1,6 @@
 import firebase from 'firebase/app'
 import DatabaseManager from './DatabaseManager'
-
-const CONFIG = {
-  apiKey: 'AIzaSyBNKpeVPa3qZ6oJZ4w9iPjKUpxKX3tB9QI',
-  authDomain: 'plant-pal-app.firebaseapp.com',
-  databaseURL: 'https://plant-pal-app.firebaseio.com',
-  projectId: 'plant-pal-app',
-  storageBucket: 'plant-pal-app.appspot.com',
-  messagingSenderId: '542248820814',
-  appId: '1:542248820814:web:fac3faac1267a891cc631f',
-  measurementId: 'G-F1VTE91K0F',
-}
+import { CONFIG } from './firebaseConfig'
 
 let initialized = false
 
