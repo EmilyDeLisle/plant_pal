6fb8f5b305bdd36ae6f8259469f05da2738716a6
EmilyDeLisle <emily.delisle@gmail.com>
2020-06-18 16:39:53 -0700

    Created auth manager class

---
+++ src/firebase/AuthenticationManager.ts
@@ -0,0 +1,91 @@
+import firebase, { auth, User } from 'firebase/app'
+import 'firebase/auth'
+
+export default class AuthenticationManager {
+  static instance: AuthenticationManager
+  auth: auth.Auth
+
+  /**
+   * Get an instance of AuthenticationManager.
+   * @returns {AuthenticationManager}
+   *  Instance of AuthenticationManager
+   */
+  static getInstance() {
+    if (!AuthenticationManager.instance) {
+      AuthenticationManager.instance = new AuthenticationManager()
+    }
+    return AuthenticationManager.instance
+  }
+
+  constructor() {
+    if (AuthenticationManager.instance) {
+      throw new Error('AuthenticationManager is a singleton class')
+    }
+    this.auth = firebase.auth()
+  }
+
+  // logIn(email: string, password: string, onSuccess, onError) {
+  //   this.auth
+  //     .signInWithEmailAndPassword(email, password)
+  //     .then((userCred) => {
+  //       if (onSuccess) {
+  //         let authObj = {
+  //           type: userCred.operationType,
+  //           additional: userCred.additionalUserInfo,
+  //           userName: userCred.user.displayName,
+  //           email: userCred.email,
+  //         };
+  //         onSuccess(authObj);
+  //       }
+  //     })
+  //     .catch(onError);
+  // }
+
+  signUp(email: string, password: string, 
+    onSuccess: (value: auth.UserCredential) => void | PromiseLike<void>,
+    onError: (reason: any) => PromiseLike<never>
+    ) {
+    this.auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(onSuccess)
+      .catch(onError);
+  }
+
+    signIn(email: string, password: string, 
+      onSuccess: (value: auth.UserCredential) => void | PromiseLike<void>,
+      onError: (reason: any) => PromiseLike<never>) {
+    this.auth
+      .signInWithEmailAndPassword(email, password)
+      .then((userCred) => {
+        // if (onSuccess) {
+        //   let authObj = {
+        //     type: userCred.operationType,
+        //     additional: userCred.additionalUserInfo,
+        //     userName: userCred.user.displayName,
+        //     email: userCred.email,
+        //   };
+        //   onSuccess(authObj);
+        // }
+      })
+      .catch(onError);
+  }
+
+  signOut(
+    onSuccess: (value: void) => void | PromiseLike<void>,
+    onError: (reason: any) => PromiseLike<never>
+  ) {
+    this.auth.signOut().then(onSuccess).catch(onError)
+  }
+
+  getCurrentUser(): User | null {
+    return this.auth.currentUser
+  }
+
+  resetPassword(
+    email: string,
+    onSuccess: (value: void) => void | PromiseLike<void>,
+    onError: (reason: any) => PromiseLike<never>
+  ) {
+    this.auth.sendPasswordResetEmail(email).then(onSuccess).catch(onError)
+  }
+}
