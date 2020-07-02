ee2c0c1d3e893f334bb3b138562aac512d96ec8a
EmilyDeLisle <emily.delisle@gmail.com>
2020-07-02 16:46:14 -0700

    created split sorting button

--- src/plants/plant-list/components/ListControls.tsx
+++ src/plants/plant-list/components/ListControls.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { inject, observer } from 'mobx-react'
 import Button from '@material-ui/core/Button'
+import Hidden from '@material-ui/core/Hidden'
 import Typography from '@material-ui/core/Typography'
-import { SortingButton } from './SortingButton'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
+import { useTheme } from '@material-ui/core/styles'
+import { SortingButton, SplitSortingButton } from './SortingButton'
 import { InspectorMode, SortingMode } from '../../../models'
 import MonsteraIcon from '../../../assets/MonsteraIcon'
 import { plantStore } from '../../../injectables'
@@ -16,28 +19,42 @@ export const ListControls = inject('plantStore')(
       sortingDirection,
       sortingMode,
     } = plantStore
+    const theme = useTheme()
+    const isNarrow = useMediaQuery(theme.breakpoints.down('md'))
     return (
       <div className="list-controls__container">
-        <Button
-          color="primary"
-          variant="contained"
-          startIcon={<MonsteraIcon />}
-          onClick={() => setInspectorMode(InspectorMode.ADD)}
-        >
-          Add Plant
-        </Button>
+        <Hidden mdDown>
+          <Button
+            color="primary"
+            variant="contained"
+            startIcon={<MonsteraIcon />}
+            onClick={() => setInspectorMode(InspectorMode.ADD)}
+          >
+            Add Plant
+          </Button>
+        </Hidden>
         <div className="list-controls__sorting-buttons">
           <Typography>Sort by: </Typography>
-          {Object.values(SortingMode).map((mode) => (
-            <SortingButton
-              key={mode}
-              selected={sortingMode === mode}
-              mode={mode}
-              direction={sortingDirection}
-              handleChangeMode={setSortingMode}
-              handleChangeDirection={setSortingDirection}
-            />
-          ))}
+          {isNarrow ? (
+            <div className="list-controls__split-sorting-button">
+              <SplitSortingButton
+                direction={sortingDirection}
+                handleChangeMode={setSortingMode}
+                handleChangeDirection={setSortingDirection}
+              />
+            </div>
+          ) : (
+            Object.values(SortingMode).map((mode) => (
+              <SortingButton
+                key={mode}
+                selected={sortingMode === mode}
+                mode={mode}
+                direction={sortingDirection}
+                handleChangeMode={setSortingMode}
+                handleChangeDirection={setSortingDirection}
+              />
+            ))
+          )}
         </div>
       </div>
     )
--- src/plants/plant-list/components/SortingButton.tsx
+++ src/plants/plant-list/components/SortingButton.tsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import Button from '@material-ui/core/Button'
+import ButtonGroup from '@material-ui/core/ButtonGroup'
+import ClickAwayListener from '@material-ui/core/ClickAwayListener'
+import Grow from '@material-ui/core/Grow'
+import Paper from '@material-ui/core/Paper'
+import Popper from '@material-ui/core/Popper'
+import MenuItem from '@material-ui/core/MenuItem'
+import MenuList from '@material-ui/core/MenuList'
 import DescendingIcon from '@material-ui/icons/ExpandMore'
 import AscendingIcon from '@material-ui/icons/ExpandLess'
 import { SortingMode, SortingDirection } from '../../../models'
 
 export interface SortingButtonProps {
-  selected: boolean
-  mode: SortingMode
   direction: SortingDirection
   handleChangeMode: (mode: SortingMode) => void
   handleChangeDirection: (mode: SortingDirection) => void
+}
+
+export interface SingleSortingButtonProps extends SortingButtonProps {
+  selected: boolean
+  mode: SortingMode
 }
 
 const titleMap = {
@@ -30,7 +40,7 @@ export const SortingButton = ({
   direction,
   handleChangeMode,
   handleChangeDirection,
-}: SortingButtonProps) => {
+}: SingleSortingButtonProps) => {
   return (
     <div className="sorting-button">
       <Button
@@ -48,5 +58,88 @@ export const SortingButton = ({
         {titleMap[mode]}
       </Button>
     </div>
+  )
+}
+
+
+
+export const SplitSortingButton = ({
+  direction,
+  handleChangeMode,
+  handleChangeDirection,
+}: SortingButtonProps) => {
+  const [open, setOpen] = useState(false)
+  const anchorRef = useRef<HTMLDivElement>(null)
+  const [selectedMode, setSelectedMode] = useState<SortingMode>(SortingMode.WATER)
+
+  const iconMap = {
+    [SortingDirection.ASC]: <AscendingIcon />,
+    [SortingDirection.DESC]: <DescendingIcon />,
+  }
+
+  const handleMenuItemClick = (mode: SortingMode) => {
+    handleChangeMode(mode)
+    setSelectedMode(mode)
+    setOpen(false)
+  }
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen)
+  }
+
+  const handleClose = (event: React.MouseEvent<Document, MouseEvent>) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target as HTMLElement)) {
+      return
+    }
+    setOpen(false)
+  }
+
+  return (
+    <>
+      <ButtonGroup variant="contained" color="primary" ref={anchorRef} aria-label="split button">
+        <Button onClick={handleToggle}>{titleMap[selectedMode]}</Button>
+        <Button
+          color="primary"
+          size="small"
+          aria-controls={open ? 'split-button-menu' : undefined}
+          aria-expanded={open ? 'true' : undefined}
+          aria-label="select merge strategy"
+          aria-haspopup="menu"
+          onClick={() =>
+            handleChangeDirection(
+              direction === SortingDirection.ASC ? SortingDirection.DESC : SortingDirection.ASC
+            )
+          }
+        >
+          {iconMap[direction]}
+        </Button>
+      </ButtonGroup>
+      <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{
+              transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom',
+            }}
+          >
+            <Paper>
+              <ClickAwayListener onClickAway={handleClose}>
+                <MenuList id="split-button-menu">
+                  {Object.values(SortingMode).map((mode) => (
+                    <MenuItem
+                      key={mode}
+                      selected={mode === selectedMode}
+                      onClick={() => handleMenuItemClick(mode)}
+                    >
+                      {titleMap[mode]}
+                    </MenuItem>
+                  ))}
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
+    </>
   )
 }
